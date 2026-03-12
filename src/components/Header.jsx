629d1b4789f136213ef3de4faba66eb67ed682a4
PavaniKristipati <pavanikristipati2006@gmail.com>
2026-03-12 15:09:15 +0530

    UPDATED

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,27 @@
-import "./Header.css"
-function Header(){
-    return <div className="App-Header">
-        <h1>My Store</h1>
-        <ul>
-            <li>Home</li>
-            <li>Products</li>
-            <li>Users</li>
-            <li>Login</li>
-            <li>Logout</li>
-        </ul>
-
+import "./Header.css";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <div className="App-Header">
+      <h1>My Store</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/cart">Cart</Link>
+        </li>
+        <li>
+          <Link to="/orders">Order</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
     </div>
+  );
 }
 export default Header;
